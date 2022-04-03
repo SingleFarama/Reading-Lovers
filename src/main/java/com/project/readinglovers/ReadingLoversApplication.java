d1589049bfb540359792caa2010ff5e513507231
SingleFarama <98427016+SingleFarama@users.noreply.github.com>
2022-04-03 15:01:25 -0300

    Add files via upload

---
+++ src/main/java/com/project/readinglovers/ReadingLoversApplication.java
@@ -0,0 +1,13 @@
+package com.project.readinglovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingLoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingLoversApplication.class, args);
+	}
+
+}
